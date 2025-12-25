f427a2ccc7d6a999830561c66e6a5e09c11999d2
Abdulganiy Ibrahim <abdulgaiyibrahim095@gmail.com>
2025-12-25 04:57:55 +0100

    added some iphone products and chnage the look of the website

--- javascript/Laitech.js
+++ javascript/Laitech.js
@@ -4,13 +4,23 @@ products.forEach((product) => {
     <div class="card">
       <img src="${product.image}" alt="${product.name}">
       <span class="product-name">${product.name}</span>
-      <p class="description">Apple ${product.name}, ${product.description.ram} with ${product.description.storage}</p>
-      <p class="price">${product.price}</p>
+
+      <div class=" product-specs">
+        <p>${product.specs.ram}, ${product.specs.storage}, ${product.specs.size}</p>
+      </div>
+      
+      <p class="product-price">${product.price}</p>
+
+      <div class="product-rating">
+        ⭐⭐⭐⭐⭐ 
+        <span>(5.0)</span>
+      </div>
+
       <div class="buttons">
 
         <a href="#" onclick="${product.button.onclick}" class="whatsapp-btn">${product.button.text}</a>
 
-        <button class="add-to-cart-btn js-add-to-cart">Add to Cart</button>
+        <button class="add-to-cart-btn js-add-to-cart" data-product-name="${product.name}">Add to Cart</button>
       </div>
       
     </div>
@@ -18,7 +28,7 @@ products.forEach((product) => {
 });
 
 
-// whatsapp link button js code begin.
+// THE CODES BELOW GENERATE WHATSAPP LINK FOR THE BUY NOW BUTTON.
 const phoneNumber = '2347062639160';
 
 function whatsappLink(productName, productPrice) {
@@ -26,13 +36,33 @@ function whatsappLink(productName, productPrice) {
   const whatsapplnk = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`
   window.open(whatsapplnk, '_blank');
 }
-//whatsapp link button js code end.
+//WHATSAPP LINK CODES ENDED HERE.
 
-// Add to cart button js code begin.
+// ADD TO CART FUNCTIONALITY
 document.querySelectorAll('.js-add-to-cart')
   .forEach((button) => {
     button.addEventListener('click', () => {
-      console.log('Added to cart');
-    })
-  })
+      const productName = button.getAttribute('data-product-name');
 
+      const existingItem = cart.find(item => item.productName === productName);
+
+      if (existingItem) {
+        existingItem.quantity += 1;
+      } else {
+        cart.push({
+        productName: productName,
+        quantity: 1
+      },);
+      }
+
+      updateCartCount();
+      console.log(cart);
+    });
+  });
+
+  // CODES TO UPDATE CART ITEM COUNT IN THE NAVBAR
+  function updateCartCount() {
+    const cartBadge = document.querySelector('.js-cart-quantity');
+    const totalItems = cart.reduce((total, item) => total += item.quantity, 0);
+    cartBadge.textContent = totalItems;
+  }
--- javascript/products.js
+++ javascript/products.js
@@ -1,49 +1,323 @@
 const products = [{
-  image: 'image/Iphone16problack_29389c4b-eb69-484a-9800-73161dddd768.webp',
-  name: 'Iphone 16 pro',
-  description: {
+  id: 'apple-iphone-17-256gb-0001',
+  image: 'image/iphone-17-jumia-img.jpg',
+  name: 'Iphone 17',
+  specs: {
     ram: '8GB RAM',
-    storage: '256GB Storage'
+    storage: '256GB ROM',
+    size: '6.7 inches',
   },
-  price:'₦1,800,000',
+  price:'₦1,560,000',
   button:{
-      onclick: "whatsappLink('Iphone 16 Pro', '₦1,800,000')",
+      onclick: "whatsappLink('Iphone 17', '₦1,560,000')",
       text: 'Buy Now'
     }
 }, {
-  image: 'image/Iphone16problack_29389c4b-eb69-484a-9800-73161dddd768.webp',
-  name: 'Iphone 16 pro',
-  price:'₦1,800,000',
-  description: {
-    ram: '8GB RAM',
-    storage: '128GB Storage'
+  id: 'apple-iphone-17-pro-max-512gb-0002',
+  image:'image/iphone-17pm-jumia-img.jpg',
+  name: 'Iphone 17 Pro Max',
+  specs: {
+    ram: '12GB RAM',
+    storage: '512GB ROM',
+    size: '6.9 inches',
   },
+  price:'₦2,750,000',
   button:{
-      onclick: "whatsappLink('Iphone 16 Pro', '₦1,800,000')",
+      onclick: "whatsappLink('Iphone 17 Pro Max', '₦2,750,000')",
       text: 'Buy Now'
     }
 }, {
-  image: 'image/Iphone16problack_29389c4b-eb69-484a-9800-73161dddd768.webp',
-  name: 'Iphone 16 pro',
-  price:'₦1,800,000',
-  description: {
+  id: 'apple-iphone-16-pro-max-512gb-0003',
+  image:'image/iphone-16pm-jumia-img.jpg',
+  name: 'Iphone 16 Pro Max',
+  specs: {
     ram: '8GB RAM',
-    storage: '512GB Storage'
+    storage: '512GB ROM',
+    size: '6.7 inches',
   },
+  price:'₦1,900,000',
   button:{
-      onclick: "whatsappLink('Iphone 16 Pro', '₦1,800,000')",
+      onclick: "whatsappLink('Iphone 16 Pro Max', '₦1,900,000')",
       text: 'Buy Now'
     }
 }, {
-  image: 'image/Iphone16problack_29389c4b-eb69-484a-9800-73161dddd768.webp',
-  name: 'Iphone 16 pro',
-  description: {
+  id: 'apple-iphone-15-pro-max-256gb-0004',
+  image:'image/iphone-15promax-jumia-mg.jpg',
+  name: 'Iphone 15 Pro Max',
+  specs: {
     ram: '8GB RAM',
-    storage: '256GB Storage'
+    storage: '256GB ROM',
+    size: '6.7 inches',
   },
-  price:'₦1,800,000',
+  price:'₦950,000',
   button:{
-      onclick: "whatsappLink('Iphone 16 Pro', '₦1,800,000')",
+      onclick: "whatsappLink('Iphone 15 Pro Max', '₦950,000')",
       text: 'Buy Now'
     }
-}];
+}, {
+  id: 'apple-iphone-17-pro-256gb-0005',
+  image:'image/iphone-17pro-jumia-img.jpg',
+  name: 'Iphone 17 Pro',
+  specs: {
+    ram: '8GB RAM',
+    storage: '256GB ROM',
+    size: '6.3 inches',
+  },
+  price:'₦2,250,000',
+  button:{
+      onclick: "whatsappLink('Iphone 17 Pro', '₦2,250,000')",
+      text: 'Buy Now'
+    }
+}, {
+  id: 'apple-iphone-16-256gb-0006',
+  image:'image/iphone-16-jumia-img.jpg',
+  name: 'Iphone 16',
+  specs: {
+    ram: '8GB RAM',
+    storage: '256GB ROM',
+    size: '6.1 inches',
+  },
+  price:'₦1,200,000',
+  button:{
+      onclick: "whatsappLink('Iphone 16', '₦1,200,000')",
+      text: 'Buy Now'
+    } 
+}, {
+  id: 'apple-iphone-16-plus-128gb-0006',
+  image:'image/iphone-16plus-jumia-img.jpg',
+  name: 'Iphone 16 Plus',
+  specs: {
+    ram: '8GB RAM',
+    storage: '128GB ROM',
+    size: '6.7 inches',
+  },
+  price:'₦1,350,000',
+  button:{
+      onclick: "whatsappLink('Iphone 16 Plus', '₦1,350,000')",
+      text: 'Buy Now'
+    }
+}, {
+  id: 'apple-iphone-13-pro-128gb-0006',
+  image:'image/iphone-13pro-jumia-img.jpg',
+  name: 'Iphone 13 Pro  ',
+  specs: {
+    ram: '6GB RAM',
+    storage: '128GB ROM',
+    size: '6.1 inches',
+  },
+  price:'₦590,000',
+  button:{
+      onclick: "whatsappLink('Iphone 13 Pro', '₦590,000')",
+      text: 'Buy Now'
+    }
+}, {
+  id: 'apple-iphone-13-128gb-0007',
+  image:'image/iphone-13-jumia-img.jpg',
+  name: 'Iphone 13',
+  specs: {
+    ram: '4GB RAM',
+    storage: '128GB ROM',
+    size: '6.1 inches',
+  },
+  price:'₦480,000',
+  button:{
+      onclick: "whatsappLink('Iphone 13', '₦480,000')",
+      text: 'Buy Now'
+    }
+}, {
+  id: 'apple-iphone-12-pro-max-128gb-0008',
+  image:'image/iphone-12pm-jumia-img.jpg',
+  name: 'Iphone 12 Pro Max',
+  specs: {
+    ram: '6GB RAM',
+    storage: '128GB ROM',
+    size: '6.7 inches',
+  },
+  price:'₦550,000',
+  button:{
+      onclick: "whatsappLink('Iphone 12 Pro Max', '₦550,000')",
+      text: 'Buy Now'
+    }
+}, {
+  id: 'apple-iphone-14-128gb-0009',
+  image:'image/iphone-14-jumia-img.jpg',
+  name: 'Iphone 14',
+  specs: {
+    ram: '6GB RAM',
+    storage: '128GB ROM',
+    size: '6.1 inches',
+  },
+  price:'₦700,000',
+  button:{
+      onclick: "whatsappLink('Iphone 14', '₦700,000')",
+      text: 'Buy Now'
+    }
+}, {
+  id: 'apple-iphone-13-pro-max-128gb-0010',
+  image:'image/iphone-13pm-jumia-img.jpg',
+  name: 'Iphone 13 Pro Max',
+  specs: {
+    ram: '6GB RAM',
+    storage: '128GB ROM',
+    size: '6.7 inches',
+  },
+  price:'₦670,000',
+  button:{
+      onclick: "whatsappLink('Iphone 13 Pro Max', '₦670,000')",
+      text: 'Buy Now'
+    }
+}, {
+  id: 'apple-iphone-12-pro-256gb-0011',
+  image:'image/iphone-12pro-jumia-img.jpg',
+  name: 'Iphone 12 Pro',
+  specs: {
+    ram: '6GB RAM',
+    storage: '128GB ROM',
+    size: '6.1 inches',
+  },
+  price:'₦4700,000',
+  button:{
+      onclick: "whatsappLink('Iphone 12 Pro', '₦470,000')",
+      text: 'Buy Now'
+    }
+}, {
+  id: 'apple-iphone-14-pro-max-256gb-0012',
+  image:'image/iphone-14pm-jumia-img.jpg',
+  name: 'Iphone 14 Pro Max',
+  specs: {
+    ram: '8GB RAM',
+    storage: '256GB ROM',
+    size: '6.7 inches',
+  },
+  price:'₦950,000',
+  button:{
+      onclick: "whatsappLink('Iphone 14 Pro Max', '₦950,000')",
+      text: 'Buy Now'
+    }
+}, {
+  id: 'apple-iphone-13(green)-128gb-0013',
+  image:'image/iphone-13(green)-jumia-img.jpg',
+  name: 'Iphone 13 (Green)',
+  specs: {
+    ram: '4GB RAM',
+    storage: '128GB ROM',
+    size: '6.1 inches',
+  },
+  price:'₦490,000',
+  button:{
+      onclick: "whatsappLink('Iphone 13 (Green)', '₦490,000')",
+      text: 'Buy Now'
+    }
+}, {
+  id: 'apple-iphone-12-64gb-0014',
+  image:'image/iphone-12-jumia-img.jpg',
+  name: 'Iphone 12',
+  specs: {
+    ram: '4GB RAM',
+    storage: '64GB ROM',
+    size: '6.1 inches',
+  },
+  price:'₦400,000',
+  button:{
+      onclick: "whatsappLink('Iphone 12', '₦400,000')",
+      text: 'Buy Now'
+    }
+}, {
+  id: 'apple-iphone-11pro-256gb-0015',
+  image:'image/iphone-11pro-jumia-img.jpg',
+  name: 'Iphone 11 Pro',
+  specs: {
+    ram: '4GB RAM',
+    storage: '256GB ROM',
+    size: '6.1 inches',
+  },
+  price:'₦420,000',
+  button:{
+      onclick: "whatsappLink('Iphone 11 Pro', '₦420,000')",
+      text: 'Buy Now'
+    }
+}, {
+  id: 'apple-iphone-12pro-max-(silver)-256gb-0016',
+  image:'image/iphone-12pm(silver)-jumia-img.jpg',
+  name: 'Iphone 12 Pro Max (Silver)',
+  specs: {
+    ram: '6GB RAM',
+    storage: '256GB ROM',
+    size: '6.7 inches',
+  },
+  price:'₦550,000',
+  button:{
+      onclick: "whatsappLink('Iphone 12 Pro Max (Silver)', '₦550,000')",
+      text: 'Buy Now'
+    }
+}, {
+  id: 'apple-iphone-15-128gb-0017',
+  image:'image/iphone-15-jumia-img.jpg',
+  name: 'Iphone 15',
+  specs: {
+    ram: '8GB RAM',
+    storage: '128GB ROM',
+    size: '6.1 inches',
+  },
+  price:'₦800,000',
+  button:{
+      onclick: "whatsappLink('Iphone 15', '₦800,000')",
+      text: 'Buy Now'
+    }
+}, {
+  id: 'apple-iphone-11-64gb-0018',
+  image:'image/iphone-11(64gb)-jumia-img.jpg',
+  name: 'Iphone 11',
+  specs: {
+    ram: '4GB RAM',
+    storage: '64GB ROM',
+    size: '6.1 inches',
+  },
+  price:'₦300,000',
+  button:{
+      onclick: "whatsappLink('Iphone 11', '₦300,000')",
+      text: 'Buy Now'
+    }
+}, {
+  id: 'apple-iphone-se-64gb-0019',
+  image:'image/iphone-se-3rdgen-jumia-img.jpg',
+  name: 'Iphone SE',
+  specs: {
+    ram: '3GB RAM',
+    storage: '64GB ROM',
+    size: '5.8 inches',
+  },
+  price:'₦220,000',
+  button:{
+      onclick: "whatsappLink('Iphone SE', '₦220,000')",
+      text: 'Buy Now'
+    }
+}, {
+  id: 'apple-iphone-11-pro-max-64gb-0020',
+  image:'image/iphone-11pm-jumia-img.jpg',
+  name: 'Iphone 11 Pro Max',
+  specs: {
+    ram: '4GB RAM',
+    storage: '64GB ROM',
+    size: '6.5 inches',
+  },
+  price:'₦450,000',
+  button:{
+      onclick: "whatsappLink('Iphone 11 Pro Max', '₦450,000')",
+      text: 'Buy Now'
+    }
+}, {
+  id:'iphone-XR-(64gb)-0021',
+  image:'image/iphone-xsmax(white)-jumia-img.jpg',
+  name: 'Iphone XS MAX',
+  specs: {
+    ram: '4GB RAM',
+    storage: '64GB ROM',
+    size: '6.5 inches',
+  },
+  price:'₦300,000',
+  button:{
+      onclick: "whatsappLink('Iphone XS MAX', '₦300,000')",
+      text: 'Buy Now'
+    }
+}, {}];
